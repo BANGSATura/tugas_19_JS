592eb68c54b9eb31c10cb91512a8578f9607f814
STR <saturasstotenkopf@gmail.com>
2019-12-30 15:00:43 +0700

    Tugas 19 JS
    Mengumpulkan Tugas 19 JS

---
+++ tugas19.js
@@ -0,0 +1,17 @@
+function tugas1() {
+  console.log("Semangat...".repeat(5));
+}
+tugas1();
+
+function tugas2() {
+  console.log("*".repeat(5));
+  console.log("*".repeat(15));
+  console.log("*".repeat(30));
+  console.log("*".repeat(45));
+  console.log("*".repeat(55));
+  console.log("*".repeat(45));
+  console.log("*".repeat(30));
+  console.log("*".repeat(15));
+  console.log("*".repeat(5));
+}
+tugas2();
